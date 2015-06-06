c8e216561c29a4391ed8479dff7b6b7da6b42c96
fanwei <fwshun@163.com>
2015-06-06 23:35:41 +0800

    menu selected

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,19 @@ module ApplicationHelper
       ["应用外语系","应用外语系"]
     ]
   end
+
+
+  #获取选中菜单链接index
+  def get_menu_index
+    con_name = controller.controller_path.to_sym
+    action_name = controller.action_name.to_sym
+    menu = {
+      home: {index: 0},
+      project: {about: 1, background: 2},
+      train: {notice_index: 3, notice_info: 3, apply_index: 4, apply_info: 4},
+      "devise/sessions": {"new": 8},
+      "devise/registrations": {"new": 7, create: 7}
+    }
+    menu[con_name][action_name] if menu.has_key?(con_name) and menu[con_name].has_key?(action_name)
+  end
 end
