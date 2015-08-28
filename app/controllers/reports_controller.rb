1be408465e300fc1e533add05635c2a0d554ce9a
xiaocuixt <cjw624923@126.com>
2015-08-28 13:02:40 +0800

    changed

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,7 +3,7 @@ class ReportsController < BaseController
 
   def index
   	@events = Event.all
-  	@bulletin = Event.where(classify: 1).order(created_at: :DESC).limit(7)
+  	@bulletin = Event.where(classify: 1).order(created_at: :DESC).limit(5)
     @headline = Event.where(classify: 2).order(created_at: :DESC).limit(5)
     @activities = Event.where(classify: 3).order(created_at: :DESC).limit(4)
   end
