adba56fa4aa3986cdd4e7fec68835f9b8d629b71
xiaocuixt <cjw624923@126.com>
2015-08-28 15:17:58 +0800

    limit 5

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,7 +2,7 @@ class ReportsController < BaseController
 	layout "reports"
 
   def index
-  	@events_with_image = Event.where("picture_url_file_name IS NOT NULL")
+  	@events_with_image = Event.where("picture_url_file_name IS NOT NULL").limit(5)
   	@bulletin = Event.where(classify: 1).order(created_at: :DESC).limit(5)
     @headline = Event.where(classify: 2).order(created_at: :DESC).limit(10)
     @activities = Event.where(classify: 3).order(created_at: :DESC).limit(6)
