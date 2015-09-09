676f7ec1e0e25452a86474c4b7ff5f523fd628b6
xiaocuixt <cjw624923@126.com>
2015-09-09 09:29:34 +0800

    fix hash

--- app/models/spider.rb
+++ app/models/spider.rb
@@ -2,15 +2,19 @@ require "nokogiri"
 require "mechanize"
 
 class Spider
-	TONGZHI_HASH = {}
 	BAOMING_HASH = {}
+	TONGZHI_ARRAY =[]
 	url = "http://www.hvett.com.cn/"
 	agent = Mechanize.new
   page = agent.get('http://www.hvett.com.cn/index.php')
 
   #国培通知
   page.search(".main2_td1")[0].search(".table_zcwj_list a").each do |link|
-    TONGZHI_HASH[link.text] = url + link['href'].to_s
+  	TONGZHI_HASH = {}
+    TONGZHI_HASH['link'] = url + link['href'].to_s
+    TONGZHI_HASH['text'] = link.text
+    TONGZHI_HASH['time'] = link.next_element.text
+    TONGZHI_ARRAY << TONGZHI_HASH
   end
 
   #国培报名
