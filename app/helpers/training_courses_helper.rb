36cd6dd617779e37e1204cdb466fd00e63f2750c
xiaocuixt <cjw624923@126.com>
2015-09-09 18:13:53 +0800

    applied state

--- app/helpers/training_courses_helper.rb
+++ app/helpers/training_courses_helper.rb
@@ -1,14 +1,18 @@
-module TrainingCoursesHelper
-	def state_training_course training_course
-		now = Time.now
-		start_time = training_course.start_apply_time
-	  end_time = training_course.end_apply_time
-	  if now > start_time && now < end_time
-	  	"进行中"
-	  elsif now < start_time
-	  	"未开始"
-	  elsif now > end_time
-	  	"已结束"
-	  end
-	end
-end
+# module TrainingCoursesHelper
+# 	def state_training_course training_course
+# 		now = Time.new
+# 		start_time = training_course.start_apply_time
+# 	  end_time = training_course.end_apply_time
+# 	  if now > start_time && now < end_time
+# 	  	content_tag(:a, "进行中", class: "btn btn-success", href: new_user_session_path)
+# 	  elsif now < start_time
+# 	  	content_tag(:a, "未开始", class: "btn btn-default", href: "javascript:void(0)")
+# 	  elsif now > end_time
+# 	   	content_tag(:a, "已结束", class: "btn btn-default", href: "javascript:void(0)")
+# 	  # elsif !training_course.be_applied
+# 	  # 	content_tag(:a, "暂时不能报名", class: "btn btn-danger", href: "javascript:void(0)")
+# 	  # else
+# 	  # 	content_tag(:a, "请先登录", class: "btn btn-lg btn-primary apply-btn", href: new_user_session_path)
+# 	  end
+# 	end
+# end
