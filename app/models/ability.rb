95daeb9cf42ce7a68517fadfa0a9767085f2d7e2
xiaocuixt <cjw624923@126.com>
2015-09-22 15:53:05 +0800

    add index usertaks

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -68,7 +68,7 @@ class Ability
     can [:index, :list], UserTrainingCourse
     can [:index, :list, :show], Journal
     can :manage, Task
-    can [:list, :show], UserTask
+    can [:list, :show, :index], UserTask
     can :manage, ManagerFeedback
     can [:list_by_teacher, :detail], TrainingCourse
     can [:index, :edit_profile, :update_profile, :edit_password, :update_password], Admin
