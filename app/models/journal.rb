5b66b37bd633d16fad5b3abf342fd03f33ad09c9
xiaocuixt <cjw624923@126.com>
2015-09-22 15:29:29 +0800

    journal amount

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -1,4 +1,9 @@
 class Journal < ActiveRecord::Base
   belongs_to :user
   belongs_to :training_course
+
+  #统计journal数量
+  def self.journal_amount(user_id, training_course_id)
+    self.where(user_id: user_id, training_course_id: training_course_id).length
+  end
 end
