41664b263a21490d8fb8f2205d3fc460b28601b6
xiaocuixt <cjw624923@126.com>
2015-12-04 09:04:21 +0800

    fix

--- app/models/event.rb
+++ app/models/event.rb
@@ -17,7 +17,7 @@
 
 class Event < ActiveRecord::Base
 	has_attached_file :picture_url, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/lunbo.png"
-  	
+
 	validates_attachment_content_type :picture_url, content_type: /\Aimage\/.*\Z/
 
 	enum classify: { bulletin: 1, headline: 2, activity: 3 }
