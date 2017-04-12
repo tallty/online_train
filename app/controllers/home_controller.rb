bdcfc524f45938f666d3db50ee9757cb0a858e1f
liyijie <liyijie825@gmail.com>
2017-04-12 10:05:53 +0800

    use first instead of last

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,13 @@
 class HomeController < BaseController
 	def index
 		#国培通知&报名
-		@training_notifications = Notification.where(category: "0").last(5)
-		@training_courses = TrainingCourse.where(category: "0").last(5)
+		@training_notifications = Notification.where(category: "0").first(5)
+		@training_courses = TrainingCourse.where(category: "0").first(5)
 		#骨干教师通知&报名
-		@teacher_notifications = Notification.where(category: "1").last(5)
-		@teacher_training_courses = TrainingCourse.where(category: "1").last(5)
+		@teacher_notifications = Notification.where(category: "1").first(5)
+		@teacher_training_courses = TrainingCourse.where(category: "1").first(5)
 		#专业负责人通知&报名
-		@profession_notifications = Notification.where(category: "2").last(5)
-		@profession_training_courses = TrainingCourse.where(category: "2").last(5)
+		@profession_notifications = Notification.where(category: "2").first(5)
+		@profession_training_courses = TrainingCourse.where(category: "2").first(5)
 	end
 end
